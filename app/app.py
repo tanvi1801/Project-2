f8df88dbd60bfc70977d4a9764603cdc11504e31
Rohan Sawant <rohansawantct83@gmail.com>
2021-08-13 03:48:41 +0000

    Added templates

--- app/app.py
+++ app/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html", text="text")
 
 
 if __name__ == '__main__':
